b24df5e3b573ce072d6fb67642733bd85b5bef19
Juuso Vuorinen <juvuorin@gmail.com>
2022-10-17 12:17:22 +0300

    ok

--- src/Oppilas.js
+++ src/Oppilas.js
@@ -5,8 +5,8 @@ const Oppilas = (props) => {
   return (
     <div>
       <div>{props.oppilas.nimi}
-      <input type="text" onChange={(event)=>{ props.oppilaanNimiMuuttui(event.target.value,props.index,props.luokanIndex) }}  value = {props.oppilas.nimi}/>
-
+       <input type="text" onChange={(event)=>{ props.oppilaanNimiMuuttui(event.target.value,props.index,props.luokanIndex) }}  value = {props.oppilas.nimi}/>
+ 
       </div>
       <div>{props.tieto2}
       </div>
