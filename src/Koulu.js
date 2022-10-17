38b8bedce2924251f52ba85568f061352e93308a
Juuso Vuorinen <juvuorin@gmail.com>
2022-10-17 14:09:07 +0300

    ok

--- src/Koulu.js
+++ src/Koulu.js
@@ -2,16 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Luokka from './Luokka'
 
-
-
 const Koulu = (props) => {
   return (
     <>
       <div>Koulun nimi:{props.koulu.nimi}</div>
-      <input type="text" onChange={(event)=>{ props.koulunNimiMuuttui(event.target.value) }}  value = {props.koulu.nimi}/>
+{/*       <input type="text" onChange={(event)=>{ props.koulunNimiMuuttui(event.target.value) }}  value = {props.koulu.nimi}/>
+ */}
+       <input type="text" onChange={(event)=>{ props.dispatch({type:"KOULUN_NIMI_MUUTTUI",payload:event.target.value})}}  value = {props.koulu.nimi}/>
 
       <div>Luokat:</div>
-      <div>{props.koulu.luokat.map((luokka,index) => <Luokka oppilaanNimiMuuttui={props.oppilaanNimiMuuttui} luokanIndex = {index} luokka={luokka} />)}</div>
+      <div>{props.koulu.luokat.map((luokka,index) => <Luokka dispatch={props.dispatch} luokanIndex = {index} luokka={luokka} />)}</div>
     </>
   );
 }
