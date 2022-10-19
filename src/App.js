1033fa53ea512d82d55c99ebd5583c86d04b52a7
Juuso Vuorinen <juvuorin@gmail.com>
2022-10-19 15:54:54 +0300

    ok

--- src/App.js
+++ src/App.js
@@ -50,8 +50,7 @@ function reducer(state, action) {
     }
     case 'LISÄÄ_KOULU': {
       console.log("Reduceria kutsuttiin", action)
-      const uudetKoulut = [...state.koulut,{ nimi: "oletusnimi" ,luokat:[]}]
-      return {...state, koulut:uudetKoulut}
+      return {...state, koulut:[...state.koulut,{ nimi: "oletusnimi" ,luokat:[]}]}
     }
     case 'PÄIVITÄ_TALLENNUSTILA':
       return { ...state, tallennetaanko: action.payload }
