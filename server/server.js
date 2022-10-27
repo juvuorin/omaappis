2320857c843cd1624804a4bb978fcc4744efcde7
Juuso Vuorinen <juvuorin@gmail.com>
2022-10-27 10:02:41 +0300

    ok

--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
 const fs = require('fs');
 const express = require('express')  //Jos ei toimi, niin "npm install express"
+const cors = require('cors')
 const app = express()
 const port = 8080
 
+
+app.use(cors())  //jos ei toimi, niin "npm install cors"
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  // tiedon luku asynkronisesti
-  res.send('Hello!')
+  console.log("Palvelimeen tultiin kyselemään dataa")
+  const data = fs.readFileSync('./kouludata.json', { encoding: 'utf8', flag: 'r' }); //Voi kestää useita sekunteja!
+  res.send(data)
 })
 app.post('/', (req, res) => {
-  // tiedon kirjoitus asynkronisesti  req.body antanee tarvittavan datan
-  res.send('Hello World!')
+  console.log("Palvelimeen tultiin tallentelemaan dataa")
+  fs.writeFileSync('kouludata.json', JSON.stringify(req.body));
+  res.send('Tais datan tallennus onnistua, kun tänne tultiin :)')
 })
 
 app.listen(port, () => {
