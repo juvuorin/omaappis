faee0408dbbb830e2b09a426c8bfe30b444522c8
Juuso Vuorinen <juvuorin@gmail.com>
2022-11-21 11:06:33 +0200

    ok

--- server/server.js
+++ server/server.js
@@ -188,16 +188,6 @@ const isAdmin = async (req, res, next) => {
     res.status(500).send(e)
   }
 
-
-  const token = req.headers.authorization?.split(' ')[1];
-  //Authorization: 'Bearer TOKEN'
-  if (!token) {
-    res.status(200).json({ success: false, message: "Error!Token was not provided." });
-  }
-  //Decoding the token
-  const decodedToken = jwt.verify(token, "secretkeyappearshere");
-  req.decoded = decodedToken
-  next()
 }
 
 const verifyToken = (req, res, next) => {
